551c0b9c7cac57dbd14df76c3adeb898e4211760
tpinhoda <tiagopinhods@gmail.com>
2017-03-30 17:40:28 -0300

    iniciar fuzzy knn

---
+++ Fuzzy-Ondemand.R
@@ -0,0 +1,128 @@
+library(stream)
+library(class)
+library(e1071)
+#------------------------------------------------------Variáveis do Dataset--------------------------------------------------------------------------------------
+#Separa o Conjunto de teste e o de treino 1 por 1
+TRAINING_DATASET = read.csv("../../DS_Datasets/Synthetic/Non-Stationary/Bench2_10k/Benchmark2_10000.csv")[c(TRUE, FALSE), ]
+TEST_DATASET = read.csv("../../DS_Datasets/Synthetic/Non-Stationary/Bench2_10k/Benchmark2_10000.csv")[c(FALSE, TRUE), ]
+
+TRAINING_SET_SIZE <-nrow(TRAINING_DATASET)    #Quantidade de instacias no dataset de treino
+TEST_SET_SIZE <-nrow(TEST_DATASET)            #Quantidade de instancia no dataset de teste
+NATTRIBUTES <- ncol(TRAINING_DATASET) -1      #Calcula quantidade de atributos presentes no dataset
+
+#----------------------------------------------Variáveis de inicializacao do algoritmo-----------------------------------------------------------------------------------------
+INITNUMBER <- 500            #Quantidade inicial de pontos que serão utilizados na criacao dos micro-grupos iniciais  
+MICROCLUSTER_RATIO <- 3      #Quantidade de micro-grupos máxima por classe na criacao inicial
+FRAME_MAX_CAPACITY <- 8      #Quantidade de snapshost por frame
+BUFFER_SIZE <- 600          #Quantidade de pontos a ser recebida até para q seja feito o teste no fluxo de teste
+KFIT <- 200                  #Quantidade de pontos que serão testadas
+T <- 2                       #Multiplica pela distancia do micro-grupo mais proximo para definir o limite maximo do micro-grupo inicial
+M <- 0.32                    #Porcentagem de ultimos pontos a chegar no micro-grupo
+POINTS_PER_UNIT_TIME <- 50   #Pontos que chegarao a cada 1 unidade de tempo
+MAX_ITERATIONS <- 10         #Quantidade max de iteracoes do algoritmo
+PHI <- 25*1000                #Limiar para decidir se um mcrogrupo é deletado ou merge
+P <- 1                       #Quantidade de horizontes para a classificacão
+STORE_MC <- 1                #Intervalo de tempo para armazenar um snapshot
+FUZZY_M <- 2                  #Parametro de fuzzyficação
+FUZZY_THETA <- 0.5            #Verifica se cria ou nao um novo micro-grupo baseado nesse threshold de pertinencia
+#-------------------------------------------Variáveis globais inicializadas automaticamente-------------------------------------------------------------------
+FRAME_NUMBER = round(log2(TRAINING_SET_SIZE))      #Quantidade de frames que haverá na tabela geométrica
+FRAMES = 0:(FRAME_NUMBER-1)                    #Lista dos números dos frames ordenada de forma crescente (0 - framenumber-1)
+SNAPSHOTS = lapply(FRAMES,function(frame){list(frame_number=frame,frame_slot=c())}) #Estrutura da tabela geométrica
+MC_ID <- 0                                    #Contador de id dos micro-grupos
+TIME <- 0                                     #Contador de tempo em unit * 1000
+HORIZONS <- 2^(1:FRAME_MAX_CAPACITY)*1000              #Horizontes para verificar os kfit pontos
+
+#------------------------------------------------------Inicializa funcões--------------------------------------------------------------------------------------------
+source("Utils.R")
+
+#==============================================================INÍCIO===========================================================================================
+#Fase 1 - Inicializacão - OFFLINE
+
+
+#Criar fluxo de dados do conjunto de treino e teste
+TRAINING_STREAM <- create.datastream(TRAINING_DATASET)
+TEST_STREAM <- create.datastream(TEST_DATASET)  
+
+#Dividir os INITNUMBER primeiros pontos do fluxo de treino por classe
+inicialization_points <- get_points(TRAINING_STREAM, n = INITNUMBER, class = TRUE) #Obtem os INITNUMBER primeiros pontos do fluxo
+splitted_points <- split.class(inicialization_points)                              #Separa os pontos por classe
+
+#Atualiza o tempo do sistema
+TIME <- (INITNUMBER/POINTS_PER_UNIT_TIME)*1000
+
+#Utilizar o kmeans em cada grupo de classe e retornar k microgrupos para cada classe
+set.seed(1)
+splitted_microclusters <- lapply(splitted_points, function(class_set){
+                                c(cmeans(class_set[, 1:NATTRIBUTES],centers =  MICROCLUSTER_RATIO, iter.max = 100, method = "cmeans", dist = "euclidean", m = 2),class=as.character(class_set[1,NATTRIBUTES+1]))
+                           }) 
+
+#Sumariza os grupos em micro-grupos
+MICROCLUSTERS <- sapply(splitted_microclusters, function(class_set_mc){
+                       apply(class_set_mc$centers,1,function(center){
+                             create.microcluster(center,class_set_mc$class)
+                        })                           
+                  })
+MICROCLUSTERS_SIZE = length(MICROCLUSTERS)
+
+#Calculo do limite de acão maximo de cada micro-grupo inicial
+
+#Não tem isso no FMIC (Definir uma forma soft para tratat problemas de sobreposição de micro-grupos)
+#dist_centers_matrix <- dist.microclusters(MICROCLUSTERS) #calcula a distancia entre todos os micro-grupos inciais
+
+#mc_initial_max_boundary <- T*apply(dist_centers_matrix,1,function(dists_microcluster){
+#                                        c(min( dists_microcluster[dists_microcluster!=min(dists_microcluster)] ))    
+#                                     }) 
+#==============================================================================================================================================
+
+
+#Fase 2 - Manutencão ONLINE
+remaining_points = TRAINING_SET_SIZE - INITNUMBER #pontos restantes no fluxo de treino a serem processados
+displacement = INITNUMBER + KFIT
+it <- 0
+while(remaining_points >= BUFFER_SIZE+KFIT){
+  it <- it + 1
+  cat("Buffer: ", it ,"\n")
+  remaining_points_buffer <- BUFFER_SIZE
+  points_until_store <- STORE_MC*POINTS_PER_UNIT_TIME
+  #Processa BUFFER_SIZE pontos do fluxo de treino por STORE_MC*POINTS_PER_UNIT
+  while(remaining_points_buffer > 0){
+    
+    training_points <- get_points(TRAINING_STREAM, n=points_until_store, class = TRUE)
+    
+    #Atualizacao dos micro-gupos
+    for(stream_point in get.rows(training_points)){
+     
+      class_stream_point <- as.character(stream_point[NATTRIBUTES+1]) #Classe do ponto
+      stream_point <- t(stream_point[1:NATTRIBUTES])                     #Ponto sem a classe
+  
+      #Calcular distancia e a pertinência do ponto para os microgrupos de sua classe e retorna os com pertinencia maior que 0.1
+      nearest <- nearest.microcluster(MICROCLUSTERS,stream_point,class_stream_point)
+      #Calcula o Micro-grupo com menor relevancia caso precise de merge ou delete
+      min_relevant <- find.min.relevant(MICROCLUSTERS)
+ 
+      if(is.empty(nearest))
+        check.relevance(min_relevant,stream_point,class_stream_point)
+      else{
+        max_membership <- max(nearest[,2])
+        if(max(nearest[,2]) >= FUZZY_THETA ) 
+          for(nearest_mic in get.rows(nearest))
+            add.point(nearest_mic,stream_point)
+        else
+          check.relevance(min_relevant,stream_point,class_stream_point)
+      }
+
+    }
+    
+    #salvar snapshot
+    TIME <- TIME + (STORE_MC*1000)
+    remaining_points_buffer <- remaining_points_buffer - points_until_store
+    store.snapshot(MICROCLUSTERS,TIME)
+  }
+  
+
+  #Pega os BUFFER_SIZE+displacement pontos do fluxo de treino para deixa-los no mesmo tempo
+  displacement <- KFIT
+  remaining_points <- remaining_points - (BUFFER_SIZE + KFIT)
+}
+
---
+++ Utils.R
@@ -0,0 +1,295 @@
+#============================================FUNCÕES============================================================================
+create.datastream <- function(dataset){
+  #Transformar um data set em um fluxo
+  #Cria o fluxo a partir do dataset escolhido
+  #Caso queira um fluxo infinito mude o atributo loop para TRUE
+  stream <- DSD_Memory(dataset[,c(1:NATTRIBUTES)], class=dataset[,NATTRIBUTES+1], loop = FALSE)  
+  return(stream)
+}
+
+split.class <- function(dataset){
+  #Cria uma lista de pontos separados pela classe
+  class_list <-dataset[,NATTRIBUTES+1]
+  splitted <- split(dataset,class_list)
+  return(splitted)
+}
+
+create.microcluster <- function(center,class){
+  #Cria um novo micro-grupo
+  MC_ID <<- MC_ID + 1
+  return(list(CF1x=center,CF2x=center^2,CF1t=TIME,CF2t=TIME^2,n=1,M=1,class_id=class,id=MC_ID))
+}
+
+get.centers <- function(MICROCLUSTERS){
+  #Retorna o centro de todos os micro-grupos
+  return(t(sapply(MICROCLUSTERS, function(microcluster){
+      microcluster$CF1x/microcluster$n
+  })))
+}
+
+get.class <- function(MICROCLUSTERS){
+  #Retorna o centro de todos os micro-grupos
+  return(t(sapply(MICROCLUSTERS, function(microcluster){
+      microcluster$class_id
+  })))
+}
+
+dist.microclusters <- function(MICROCLUSTERS){
+  #Retorna uma matrix de distancia entro todosos micro-grupos
+  
+  micro_clusters_centers <- get.centers(MICROCLUSTERS)
+  
+  #Calcula a distancia entre os micro-grupos
+  dist_centers <- dist(micro_clusters_centers)
+  dist_centers <- as.matrix(dist_centers)
+  
+  #renomeia as linhas e colunas
+  rownames(dist_centers) <- c(1:length(MICROCLUSTERS))
+  colnames(dist_centers) <- c(1:length(MICROCLUSTERS))
+  
+  return(dist_centers)
+}
+
+find.microclusters <-function(MICROCLUSTERS,class){
+  #retorna a lista de ids de micro-grupos da mesma classe
+  ids_class <- c()
+  for(microcluster_index in 1:MICROCLUSTERS_SIZE)
+    if(MICROCLUSTERS[[microcluster_index]]$class_id == class)
+       ids_class <- c(ids_class,microcluster_index)
+  return(ids_class)  
+}
+
+get.distances <- function(MICROCLUSTERS,point){
+  microclusters_centers <- get.centers(MICROCLUSTERS)              #Recupera os centros CF1x/n de todos os microclusters
+  distances <- apply(microclusters_centers,1,function(centers){    #Calcula a distancia do novo ponto para todos os microclusters
+                    dist(rbind(centers, point))
+                })
+  return(distances)
+}
+is.empty <- function(x) return(length(x) == 0)
+
+calculate.membership <- function(distances,mic_size){
+  membership <- numeric()
+  ## Calculating membership values for p in all availableFMiC
+  for(j in 1:mic_size){
+    s <- 0
+    for(k in 1:mic_size) {
+      if(distances[j] > 0 && distances[k]>0)
+        s <- s + ((distances[j]/distances[k])^(2/(FUZZY_M-1)))
+    }
+    if(s > 0)
+      membership <- c(membership, 1/s)
+    else
+      membership <- c(membership, s)
+  }
+  return(membership)
+}
+
+nearest.microcluster <- function(MICROCLUSTERS,point,class){
+  class_microclusters <- find.microclusters(MICROCLUSTERS,class)
+  if(is.empty(class_microclusters))
+    return(class_microclusters)
+  point_distances <- get.distances(MICROCLUSTERS,point)
+  point_memberships <- calculate.membership(point_distances,MICROCLUSTERS_SIZE)
+
+  thresholded_memberships <- point_memberships > 0.1
+
+  index_mics <- 1:MICROCLUSTERS_SIZE
+  nearest_index <- index_mics[thresholded_memberships]
+  nearest_dist <-  point_memberships[thresholded_memberships]
+  nearests <- cbind(index = nearest_index,membership = nearest_dist)
+  return(nearests)
+}
+
+
+mean.timestamp <- function(microcluster){
+  points_number <- microcluster$n
+  std_timestamp <- sqrt((microcluster$CF2t/microcluster$n)-(microcluster$CF1t/points_number)^2)
+  mean_timestamp <- microcluster$CF1t/points_number
+  
+  if(microcluster$n < 2*M*points_number){
+    return(mean_timestamp)
+  }else
+    mean_timestamp <- qnorm(((M*points_number)/(2*points_number)),mean_timestamp,std_timestamp) 
+  return(mean_timestamp)
+    
+}
+
+find.min.relevant <- function(MICROCLUSTERS){
+  mean_timestamps <- sapply(MICROCLUSTERS,function(microcluster){mean.timestamp(microcluster)})
+  min_relevance <- min(mean_timestamps)
+  min_relevant_index <- which.min(mean_timestamps)
+  return(c(index = min_relevant_index, relevance = min_relevance))
+}
+
+
+check.relevance <- function(min._relevant, point_stream, class){
+  if(min_relevant['relevance'] < PHI)
+    delete.microcluster(min_relevant['index'], point_stream,class)
+  else{
+    merge.microclusters(min_relevant,point_stream,class)
+    delete.microcluster(min_relevant['index'],point_stream,class)
+  }  
+}
+
+delete.microcluster <- function(microcluster_index,point,class){
+
+  MICROCLUSTERS[[microcluster_index]]$CF1x <<-  point
+  MICROCLUSTERS[[microcluster_index]]$CF2x <<-  point*point
+  MICROCLUSTERS[[microcluster_index]]$CF1t <<-  TIME
+  MICROCLUSTERS[[microcluster_index]]$CF2t <<-  TIME^2
+  MICROCLUSTERS[[microcluster_index]]$n <<-  1
+  MICROCLUSTERS[[microcluster_index]]$class_id <<- class
+  MC_ID <<- MC_ID + 1
+  MICROCLUSTERS[[microcluster_index]]$id <<- MC_ID
+  M <<- 1
+  
+  #Calculo do limite de acão maximo de cada micro-grupo inicial
+  
+}
+
+
+sum.microclusters <- function(mc1,mc2){
+
+  MICROCLUSTERS[[mc1]]$CF1x <<-  MICROCLUSTERS[[mc1]]$CF1x + MICROCLUSTERS[[mc2]]$CF1x
+  MICROCLUSTERS[[mc1]]$CF2x <<-  MICROCLUSTERS[[mc1]]$CF2x + MICROCLUSTERS[[mc2]]$CF2x  
+
+  MICROCLUSTERS[[mc1]]$M <<-  MICROCLUSTERS[[mc1]]$M + MICROCLUSTERS[[mc2]]$M
+  MICROCLUSTERS[[mc1]]$n <<-  MICROCLUSTERS[[mc1]]$n + MICROCLUSTERS[[mc2]]$n
+  MICROCLUSTERS[[mc1]]$id <<- c(MICROCLUSTERS[[mc1]]$id,MICROCLUSTERS[[mc2]]$id )
+  
+  if(MICROCLUSTERS[[mc2]]$CF1t > MICROCLUSTERS[[mc1]]$CF1t)
+    MICROCLUSTERS[[mc1]]$CF1t <- MICROCLUSTERS[[mc2]]$CF1t
+  if(MICROCLUSTERS[[mc2]]$CF2t > MICROCLUSTERS[[mc1]]$CF2t)
+    MICROCLUSTERS[[mc1]]$CF2t <- MICROCLUSTERS[[mc2]]$CF2t
+  
+}
+
+merge.microclusters <- function(min_relevant,point,class){
+  min_relevant_mic <- MICROCLUSTERS[[min_relevant['index']]]
+  min_relevant_class <- min_relevant_mic$class_id
+  min_relevant_center <- min_relevant_mic$CF1x/min_relevant_mic$n
+  class_microclusters <- find.microclusters(MICROCLUSTERS,min_relevant_class)
+  if(is.empty(class_microclusters))
+    delete.microcluster(min_relevant,point,class)
+  else{
+    point_distances <- get.distances(MICROCLUSTERS,min_relevant_center)
+    point_memberships <- calculate.membership(point_distances,MICROCLUSTERS_SIZE-1)
+    thresholded_memberships <- point_memberships > 0.1
+    index_mics <- 1:MICROCLUSTERS_SIZE
+    nearest_index <- index_mics[thresholded_memberships]
+    
+    for(index in nearest_index )
+      sum.microclusters(index,min_relevant['index'])
+  }
+  
+}
+
+get.maxboundary <- function(MICROCLUSTERS,mic_index){
+  mic <- MICROCLUSTERS[[mic_index]]
+  if(mic$n == 1)
+    return(mc_initial_max_boundary[mic_index])
+  rmsd <- sqrt(sum(mic$CF2x)/mic$n - sum(mic$CF1x^2)/mic$n^2)
+  return(T*rmsd)
+}
+
+add.point <- function(microcluster,point){
+  microcluster_index <- microcluster[1]
+  microcluster_membership <- microcluster[2]
+  MICROCLUSTERS[[microcluster_index]]$CF1x <<- MICROCLUSTERS[[microcluster_index]]$CF1x + point*microcluster_membership
+  MICROCLUSTERS[[microcluster_index]]$CF2x <<- MICROCLUSTERS[[microcluster_index]]$CF2x + (point^2)*microcluster_membership
+  MICROCLUSTERS[[microcluster_index]]$CF1t <<- MICROCLUSTERS[[microcluster_index]]$CF1t + TIME
+  MICROCLUSTERS[[microcluster_index]]$CF2t <<- MICROCLUSTERS[[microcluster_index]]$CF2t + TIME^2
+  MICROCLUSTERS[[microcluster_index]]$n <<- MICROCLUSTERS[[microcluster_index]]$n + 1
+  MICROCLUSTERS[[microcluster_index]]$M <<- MICROCLUSTERS[[microcluster_index]]$M + microcluster_membership
+  
+  
+}
+
+get.rows = function(training_points) lapply(seq_len(nrow(training_points)),function(index) return(data.matrix(training_points[index,])))
+
+mod<-function(x,m){ 
+  t1<-x/m
+  return(x-t1*m)
+}
+
+store.snapshot <- function(MICROCLUSTERS, TIME){
+  for(frame_number in FRAMES){
+    frame_index <- frame_number + 1
+    if((TIME%%2^frame_number==0) & (TIME%%2^(frame_number+1)>0))  {
+      if(as.numeric(length(SNAPSHOTS[[frame_index]]$frame_slot)) < FRAME_MAX_CAPACITY){
+        SNAPSHOTS[[frame_index]]$frame_slot <<- c(SNAPSHOTS[[frame_index]]$frame_slot,list(list(MICROCLUSTERS = MICROCLUSTERS, time = TIME)))
+      }else{
+        SNAPSHOTS[[frame_index]]$frame_slot <<- c(SNAPSHOTS[[frame_index]]$frame_slot[2:FRAME_MAX_CAPACITY],list(list(MICROCLUSTERS = MICROCLUSTERS, time = TIME))) 
+      }
+    }  
+  }
+}
+
+
+get.centersclass <- function(MICROCLUSTERS){
+  return(t(sapply(MICROCLUSTERS, function(microcluster){
+    c(as.numeric(microcluster$CF1x/microcluster$n),microcluster$class_id)
+  })))
+}
+
+
+find.snapshot <- function(t){
+  while(t >= 0){
+    for(fr in FRAMES+1){
+      snaps <- 1
+      while(snaps < length(SNAPSHOTS[[fr]]$frame_slot)){
+        if(SNAPSHOTS[[fr]]$frame_slot[[snaps]]$time == t){
+          return(SNAPSHOTS[[fr]]$frame_slot[[snaps]]$MICROCLUSTERS)
+        }
+        snaps <- snaps + 1
+      }
+    }
+    t <- t - 1
+  }  
+}
+
+relating.microcluster <- function(time,horizon) {
+  mc1 <- find.snapshot(time)
+  mc2 <- find.snapshot(time-horizon)
+  idmc1 <- 1
+  while(idmc1 < length(mc1)){
+    idmc2 <- 1
+    while(idmc2 < length(mc2)){
+      if(length(setdiff(mc1[[idmc1]]$id,mc2[[idmc2]]$id))<length(mc1[[idmc1]]$id)){
+        mc1[[idmc1]]$CF1x <- mc1[[idmc1]]$CF1x - mc2[[idmc2]]$CF1x
+        mc1[[idmc1]]$CF2x <- mc1[[idmc1]]$CF2x - mc2[[idmc2]]$CF2x
+        mc1[[idmc1]]$CF1t <- mc1[[idmc1]]$CF1t - mc2[[idmc2]]$CF1t
+        mc1[[idmc1]]$CF2t <- mc1[[idmc1]]$CF2t - mc2[[idmc2]]$CF2t
+        mc1[[idmc1]]$n <- mc1[[idmc1]]$n - mc2[[idmc2]]$n
+        mc1[[idmc1]]$CF1t <- setdiff(mc1[[idmc1]]$id,mc2[[idmc2]]$id)
+      }  
+      idmc2 <- idmc2 + 1
+    }
+    idmc1 <- idmc1 + 1
+  }  
+  return(mc1)
+}
+
+eucDist <- function(x1, x2){ return(sqrt(sum((x1 - x2) ^ 2))) }
+
+calculate.accuracy <- function(y_pred,label){
+  index_label <- 1
+  right <- 0
+  for(y in y_pred){
+    if(y == label[index_label])
+      right <- right + 1
+    index_label <- index_label + 1
+  }
+  return(right/index_label)
+}
+
+get.besthorizons <- function(HORIZONS_FITTING,P){
+  accuracy <- sapply(HORIZONS_FITTING,function(horizon){horizon$accuracy})
+  best_accuracy <- order(accuracy,decreasing = T)[1:P]
+  best_horizons <- c()
+  for(index in best_accuracy){
+    best_horizons <- c(best_horizons,list(HORIZONS_FITTING[[index]]))
+  }
+  return(best_horizons)
+}
